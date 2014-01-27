c9d5de5eedceeff7b72fd0394c4102105fc3a2fc
Erik Peterson <Erik.Exists@gmail.com>
2014-01-26 18:32:23 -0600

    Added fixed_point to vis, untested

--- vis.py
+++ vis.py
@@ -71,3 +71,47 @@ def phase_diagram(xfix=(), xstable=(), size=60, offset=12):
     print(''.join(line))
     print(''.join(annote))
     
+
+def fixed_point(xfix=(), size=60, offset=12):
+    """Display a (text) fixed point diagram
+
+    Parameters
+    ---------
+    xfix : tuple
+        The fixed points
+    size : int (size > 20)
+        Width of the phase line in characters
+    offset : int (offset > 2; size*.1 < offset < size*.25)
+        Min/max offset of fixed points on the line
+    """
+    
+    n = len(xfix)
+    xfix = list(xfix)
+
+    if n < 1:
+        print("No fixed points")
+        return None
+    
+    # Set some (hopefully) sane display limits
+    if size < 20:
+        raise ValueError("size must be greater than 20")
+    if offset/float(size) < 0.1:
+        raise ValueError("offset must be 10 percent of size")
+    if offset/float(size) > 0.25:
+        raise ValueError("offset must be less then 25 percent of size")
+    if offset < 1:
+        raise ValueError("offset must be > 1")        
+
+    # Init a phase linees and its annotations
+    line = ["-", ] * size
+    annote = [" ", ] * size
+
+    # Add fixed points
+    steps = range(offset, size-offset, (size - 2*offset)/n)
+    for s, xf in zip(steps, xfix):
+        line[s] = "*"
+        annote[s] = str(xf)
+
+    print("\n")
+    print(''.join(line))
+    print(''.join(annote))
